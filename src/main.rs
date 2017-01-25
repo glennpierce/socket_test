ea157ecaf087ab510aca77fc3a85cca4db72c50a
Glenn Pierce <glenn.pierce@carnegosystems.com>
2017-01-25 11:27:47 +0000

    Basic client server implemented

--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,41 @@
-//#![deny(warnings)]
+//TODO
+//spawn(bmos_http_server::serve);
+
 extern crate hyper;
 extern crate env_logger;
-
-use std::thread::spawn;
-
-mod bmos_http_server;
-
+extern crate byteorder;
 extern crate mio;
+extern crate slab;
+#[macro_use] extern crate log;
+
+mod bmos_server;
+mod bmos_connection;
+
+use std::net::SocketAddr;
+
 use mio::*;
+use mio::tcp::*;
 
-struct WebSocketServer;
-
-impl Handler for BmosServer {
-    // Traits can have useful default implementations, so in fact the handler
-    // interface requires us to provide only two things: concrete types for
-    // timeouts and messages.
-    // We're not ready to cover these fancy details, and we wouldn't get to them
-    // anytime soon, so let's get along with the defaults from the mio examples:
-    type Timeout = usize;
-    type Message = ();
-}
+use bmos_server::*;
 
 fn main() {
 
-    //spawn(bmos_http_server::serve);
+    // Before doing anything, let us register a logger. The mio library has really good logging
+    // at the _trace_ and _debug_ levels. Having a logger setup is invaluable when trying to
+    // figure out why something is not working correctly.
+    env_logger::init().expect("Failed to init logger");
 
-    let mut event_loop = EventLoop::new().unwrap();
-    // Create a new instance of our handler struct:
-    let mut handler = BmosServer;
-    // ... and provide the event loop with a mutable reference to it:
-    event_loop.run(&mut handler).unwrap();
+    let addr = "127.0.0.1:8000".parse::<SocketAddr>()
+        .expect("Failed to parse host:port string");
+    let sock = TcpListener::bind(&addr).expect("Failed to bind address");
+
+    // Create a polling object that will be used by the server to receive events
+    let mut poll = Poll::new().expect("Failed to create Poll");
+
+    // Create our Server object and start polling for events. I am hiding away
+    // the details of how registering works inside of the `Server` object. One reason I
+    // really like this is to get around having to have `const SERVER = Token(0)` at the top of my
+    // file. It also keeps our polling options inside `Server`.
+    let mut server = Server::new(sock);
+    server.run(&mut poll).expect("Failed to run server");
 }
